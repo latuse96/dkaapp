f12fd5278d165d59f6616ccbb0e731ef584c7ebb
John Kasper <kaspej12@uwosh.edu>
2019-04-25 13:54:07 -0500

    Updates to DKA table

--- 481_project/main.js
+++ 481_project/main.js
@@ -107,7 +107,7 @@ $(function () {
     };
 
     populateTable = function() {
-        var kgWeight = $("#cwKg").val();
+        var kgWeight = sessionStorage.cwKg;
         var maintenanceFluid;
         if(kgWeight <= 10) {
             maintenanceFluid = kgWeight * 100;
@@ -121,12 +121,14 @@ $(function () {
         }
         var dropFactor = 20;
         var infusionRate = (maintenanceFluid/1440 * dropFactor);
+        var insulinInfusionUnits = 0.1 * kgWeight;
 
         $(".fullRate").text(infusionRate.toFixed(2));
         $(".halfRate").text((infusionRate * .5).toFixed(2));
         $(".threeQuarterRate").text((infusionRate * .75).toFixed(2));
         $(".quarterRate").text((infusionRate * .25).toFixed(2));
         $(".zeroRate").text(0);
+        $(".infusionRate").text(insulinInfusionUnits.toFixed(2));
     }
 
     infoPrompt = function(message) {
